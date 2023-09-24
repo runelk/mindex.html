6933e099b8abf98c77cf1e405f72943cd1905b8e
runelk <r.l.knudsen909@gmail.com>
2023-09-24 03:05:46 +0200

    Add a custom element and a util function for string templates.

---
+++ elements/mindex-base.element.js
@@ -0,0 +1,25 @@
+import { h } from "../utils.js"
+
+class MindexBaseElement extends HTMLElement {
+  name = "Mindex Base Element"
+  style = h`
+    <style>
+      :host {
+        display: block;
+      }
+    </style>
+  `
+  html = h`
+    ${ this.style }
+    <h2>${ this.name }</h2>
+    <slot></slot>
+    <p>apsoidjosijioj o fso s</p>
+  `
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.innerHTML = this.html
+  }
+}
+
+customElements.define('mindex-base', MindexBaseElement)
---
+++ utils.js
@@ -0,0 +1,19 @@
+/**
+ * Template string to prepare HTML strings.
+ * 
+ * Usage:
+ * const title = 'My Title';
+ * const description = 'My Description';
+ * 
+ * const html = h`
+ *  <div class="container">
+ *   <h1>${title}</h1>
+ *  <p>${description}</p>
+ * </div>
+ * 
+ */
+function h(s, ...args) {
+  return s.reduce((acc, cur, i) => acc + cur + (args[i] || ''), '')
+}
+
+export { h }
